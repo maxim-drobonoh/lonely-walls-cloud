4aa1724d0ee985dc5534560e1a7c8e7e5b158c66
Maxim Drobonoh <maxim.drobonoh@gmail.com>
2021-02-16 19:39:45 +0200

    feature: generate search keywords for artwork onCreate event

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,41 @@
-import * as functions from "firebase-functions";
+import functions = require("firebase-functions");
+import admin = require("firebase-admin");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.onArtworkCreate = functions.firestore.document("artworks/{artworkId}")
+    .onCreate((snap) => {
+      const artworkId = snap.id;
+      const artwork = snap.data();
+
+      if ( !artwork.title || !artwork.userName || !artwork.category) return;
+
+      const searchKeywords = generateKeywords(artwork.title)
+          .concat(generateKeywords(artwork.userName))
+          .concat(generateKeywords(artwork.category));
+
+      const indexArtwork = {...artwork, searchKeywords: searchKeywords};
+
+      return db.collection("artworks")
+          .doc(artworkId)
+          .set(indexArtwork, {merge: true});
+    });
+
+const generateKeywords = (fieldValue: string) => {
+  const wordArr = fieldValue.toLowerCase().split(" ");
+  const searchableKeywords = [];
+
+  let prevKey = "";
+  for (const word of wordArr) {
+    const charArr = word.toLowerCase().split("");
+    for (const char of charArr) {
+      const keyword = prevKey + char;
+      searchableKeywords.push(keyword);
+      prevKey = keyword;
+    }
+    prevKey = "";
+  }
+  return searchableKeywords;
+};
